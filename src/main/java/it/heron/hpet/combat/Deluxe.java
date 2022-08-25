95f48469e35b8a0ca3767e072f1b6ea545c08305
Heron4gf <billievienno@gmail.com>
2022-08-25 22:33:03 +0200

    + update log on spigot

--- src/main/java/it/heron/hpet/combat/Deluxe.java
+++ src/main/java/it/heron/hpet/combat/Deluxe.java
@@ -1,20 +1,37 @@
 package it.heron.hpet.combat;
 
 import it.heron.hpet.Pet;
-import nl.marido.deluxecombat.events.CombatlogEvent;
+import it.heron.hpet.pettypes.PetType;
+import nl.marido.deluxecombat.events.CombatStateChangeEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class Deluxe implements Listener {
 
     @EventHandler
-    void onCombat(CombatlogEvent event) {
-        removePet(event.getCombatlogger());
-        removePet(event.getLastAttacker());
+    void onCombat(CombatStateChangeEvent  event) {
+        switch(event.getState()) {
+            case TAGGED:
+                removePet(event.getPlayer());
+                break;
+            case UNTAGGED:
+                Pet.getApi().selectPet(event.getPlayer(), disabledPet.get(event.getPlayer().getUniqueId()));
+                disabledPet.remove(event.getPlayer().getUniqueId());
+                break;
+        }
     }
+
+    private Map<UUID, PetType> disabledPet = new HashMap<>();
     private void removePet(Player p) {
-        if(Pet.getApi().hasUserPet(p)) Pet.getApi().getUserPet(p).remove();
+        if(Pet.getApi().hasUserPet(p)) {
+            disabledPet.put(p.getUniqueId(), Pet.getApi().getUserPet(p).getType());
+            Pet.getApi().getUserPet(p).remove();
+        }
     }
 
 }
